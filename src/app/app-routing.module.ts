a5c065afaa12b05e1f3585e45a0ce57461c91671
Dave <dave@webdave.de>
2019-02-27 20:47:52 +0100

    service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    loadChildren: './books/books.module#BooksModule'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/about'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
