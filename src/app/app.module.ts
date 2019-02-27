89bcb15794fb01d99f59ee792cbf410471f254e3
Dave <dave@webdave.de>
2019-02-27 21:26:56 +0100

    use interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { AboutModule } from './about/about.module';
+import { ErrorInterceptor } from './books/shared/error.interceptor';
 
 @NgModule({
   declarations: [AppComponent, MyNavComponent],
   imports: [BrowserModule, AppRoutingModule, AboutModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
